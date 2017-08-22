0485fb8272a30dc9e5c04ca141808b4d4beda3e8
jpushpendradolphin <anjanpushpendra@gmail.com>
2017-08-22 12:02:37 +0530

    modified....

--- sahajjobs/src/main/java/com/pusp/dao/LoginServiceIImpl.java
+++ sahajjobs/src/main/java/com/pusp/dao/LoginServiceIImpl.java
@@ -3,7 +3,6 @@ package com.pusp.dao;
 import java.io.File;
 import java.io.FileReader;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
